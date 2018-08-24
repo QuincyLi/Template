913d9e640ff6d3a0aa10b9c13ead5ee5628a303e
李权 <466217509@qq.com>
2018-08-24 16:51:47 +0800

    注册store，新增action和reducers文件

--- src/Router/Entry.js
+++ src/Router/Entry.js
@@ -1,1 +1,17 @@
 import React, { Component } from 'react';
+import { Provider, connect } from 'react-redux';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import Store from '../Store/Store';
+
+class Entry extends Component {
+  render(){
+    return (
+      <Provider store={Store}>
+        
+      </Provider>
+    );
+  }
+}
+
+export default Entry;
