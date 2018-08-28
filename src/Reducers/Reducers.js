ff62ec1590febffbfa8800d49eea4055881c7a3a
Quincy <466217509@qq.com>
2018-08-28 09:53:41 +0800

    去除console.log

--- src/Reducers/Reducers.js
+++ src/Reducers/Reducers.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import { GETTOKEN, GOTTOKEN } from '../Actions/Actions';
 
 function getToken(defaultAction = GETTOKEN , action) {
-  console.log(defaultAction, action);
   switch (action.type) {
     case GETTOKEN:
       return { type: GETTOKEN };
