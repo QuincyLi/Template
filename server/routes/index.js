8b1e1b7af1adc055b386d1859366f8672622bda4
Quincy <466217509@qq.com>
2018-08-29 14:27:53 +0800

    token更新

--- server/routes/index.js
+++ server/routes/index.js
@@ -30,16 +30,21 @@ router.post('/getToken', async (ctx) => {
   //   refreshToken = JSON.parse(body).refreshToken;
   //   ctx.body = JSON.stringify({ accessToken, refreshToken, expiresIn });
   // });
-  const url = `http://10.30.55.101/api/auth/oauth2/token?grantType=client_credential&appid=${license.appid}&secret=${license.secret}`;
+  const url = `http://10.30.52.25:8080/api/auth/oauth2/token?grantType=client_credential&appid=${license.appid}&secret=${license.secret}`;
   try {
-    ctx.body = await request.get(url);
+    const token = JSON.parse(await request.get(url));
+    accessToken = token.accessToken;
+    refreshToken = token.refreshToken;
+    expiresIn = token.expiresIn;
+    ctx.body = JSON.stringify({ accessToken, expiresIn, refreshToken });
   } catch (e) {
     ctx.body = JSON.stringify(e);
   }
 });
 
 router.post('/getAppList', async ctx => {
-  ctx.body = await request.get('http://10.30.52.63:8080/api/compose/manage/app?');
+  const url = `http://10.30.52.25:8080/api/compose/manage/apps?accessToken=${accessToken}&expiresIn=${expiresIn}&refreshToken=${refreshToken}`;
+  ctx.body = await request.get(url);
 });
 
 export default router;
